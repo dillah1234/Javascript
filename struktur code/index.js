73b4f6014b4dff20cf19df8a2dcbb986453fad48
dillah1234 <arrafif0722@abudzar.sch.id>
2024-05-07 13:58:24 +0700

    index.js
    praktek

---
+++ struktur code/index.js
@@ -0,0 +1,43 @@
+var hasil_perkalian = 6 * 6;
+console.log(hasil_perkalian);
+// soal andi
+var nama='Javascript'
+console.log(nama);
+
+var hargabuku='30000'
+console.log(hargabuku);
+
+var jumlahdibeli=5
+console.log(jumlahdibeli);
+
+var totalharga= jumlahdibeli*hargabuku
+console.log(totalharga)
+//soal andi 2
+var namabuku='kitab tauhid'
+var hargabuku=150000
+var jumlahdibeli=10
+var discount = 0.5
+var totaldiscount=totalharga*discount
+var totalharga=hargabuku*jumlahdibeli-discount
+console.log('ini buku '+namabuku+' jumlah beli '+jumlahdibeli+' total '+totalharga+' discount '+totaldiscount)
+var totalhargasetelahdiscount=totalharga*discount
+console.log(' Total harga setelah discount '+totalhargasetelahdiscount)
+// soal andi 3
+var buku_1='fathul izar'
+var buku_2='qiroatul uyun '
+var hargabuku_1=100000
+var hargabuku_2=200000
+var jumlahdibeli_1=5
+var jumlahdibeli_2=7
+var totalbuku_1 =hargabuku_1*jumlahdibeli_1
+console.log(totalbuku_1)
+var totalbuku_2=hargabuku_2*jumlahdibeli_2
+console.log(totalbuku_2)
+var discount=0.7
+var totaldiscount_1=hargabuku_1*jumlahdibeli_1-discount
+console.log(totaldiscount_1)
+
+var totalharga=totalbuku_1+totalbuku_2-discount
+console.log(totalharga)
+console.log('ini buku '+buku_1+' dan buku '+buku_2+totalharga)
+var totalhargasetelahdiscount_1
