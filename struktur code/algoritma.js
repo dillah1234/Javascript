00522282f77ae11e7d1f414cbda3e80becaa63e7
dillah1234 <arrafif0722@abudzar.sch.id>
2024-05-09 13:09:59 +0700

    algoritma

---
+++ struktur code/algoritma.js
@@ -0,0 +1,66 @@
+let permen=30;
+let budi=10;
+let hasil=permen - budi;
+console.log(hasil) 
+let permen2=40
+let hasil2=40 * 40
+console.log(hasil2) // nomer 1
+
+let kucing = "ali" + "hasan"
+console.log(kucing) // nomer 2
+
+let belalang='12'
+let nyamuk='10'
+let angsa=10
+console.log(+belalang);
+let hasilbelalangnyamuk = +belalang+nyamuk
+console.log(hasilbelalangnyamuk)
+
+let hasilnyamukangsa = +nyamuk+angsa
+console.log(hasilnyamukangsa)
+
+let bukufiksi=5000
+let ygdibeli=5
+let totalBukuFiksi=bukufiksi*ygdibeli
+console.log(totalBukuFiksi)
+
+let bukunonfiksi=7000
+let ygdibeli2=3
+let totalBukuNonFiksi=bukunonfiksi*ygdibeli2
+console.log(totalBukuNonFiksi);
+let pena=2000
+let ygdibeli3=2
+totalPena=pena*ygdibeli3
+console.log(totalPena);
+
+let totalygharusdibayaraisyah= totalBukuFiksi+totalBukuNonFiksi+totalPena
+console.log(totalygharusdibayaraisyah); //nomer 4
+
+let x =5 + 3 * 2
+console.log(x);
+
+let kresek= 4
+let kotak=6
+let ember=15
+
+let jumlah=ember+kresek+kotak
+console.log(jumlah); //nomer 6
+
+let z = 5+10+15
+let c = z;
+let y = z;
+console.log(z); // nomer 7
+
+let n = 4
+let r = n - 2
+let b = n * 2 // nomer 8
+
+let counter = 5
+counter++
+console.log(counter);
+
+let counters = 8
+counters--
+console.log(counters); // nomer 9
+
+
